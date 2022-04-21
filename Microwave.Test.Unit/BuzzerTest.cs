e5c36fa56abc220918f1018d724b236e0f13eb32
Andreas Høgh <au639339@uni.au.dk>
2022-04-21 13:20:17 +0200

    buzzer test

--- Microwave.Test.Unit/BuzzerTest.cs
+++ Microwave.Test.Unit/BuzzerTest.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace Microwave.Test.Unit
 {
     class BuzzerTest
     {
+        private Buzzer uut;
+        private IOutput output;
+        private IUserInterface ui;
+        private ICookController cookController;
+
+        [SetUp]
+        public void Setup()
+        {
+            output = Substitute.For<IOutput>();
+            ui = Substitute.For<IUserInterface>();
+            cookController = Substitute.For<ICookController>();
+
+
+
+            uut = new Buzzer(output);
+        }
+
+        [Test]
+        public void BuzzerSound_When_CookingIsDone()
+        {
+            uut.StartBuzzing();
+            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("BEEP BEEP BEEP")));
+        }
     }
 }
